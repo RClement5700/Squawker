1fe223222fc67476267d3c295f86372a5eaaa3bc
RClement5700 <Rclement5700@gmail.com>
2020-04-18 18:42:51 -0700

    running

--- app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -17,31 +17,27 @@ package android.example.com.squawker.fcm;
 
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 /**
  * Listens for changes in the InstanceID
  */
-public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService {
+public class SquawkFirebaseInstanceIdService extends FirebaseMessagingService {
 
     private static String LOG_TAG = SquawkFirebaseInstanceIdService.class.getSimpleName();
 
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the InstanceID token
-     * is initially generated so this is where you would retrieve the token.
-     */
     @Override
-    public void onTokenRefresh() {
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
         // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+        Log.d(LOG_TAG, "Refreshed token: " + s);
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken);
+        sendRegistrationToServer(s);
     }
 
     /**
